3f6ae87d01de00bfbfa94076c1626499a331085b
Denilson Rocha <eu.denilsonrocha@gmail.com>
2020-10-04 16:57:44 -0300

    app 1.0

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h1 class="tittle mb-5"> POKEBOOK</h1>
+    <b-input-group  class="mb-5">
+      <b-input-group-prepend is-text>
+        <b-icon icon="search" @click="buscar"></b-icon>
+      </b-input-group-prepend>
+      <b-form-input type="search" placeholder="pesquisar pokémon" v-model="busca" @keyup.enter="buscar"></b-form-input>
+    </b-input-group>
+
+      <div class="row">
+        <div  v-for="(poke,index) in filteredPokemons" :key="poke.url" class="col">
+        <Pokemond :name="poke.name" :url="poke.url" :num="index +1"/>
+      </div>
+      </div>
+      
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+ import Pokemond from './components/Pokemon'
+ import axios from 'axios';
 export default {
   name: 'App',
+  data(){
+    return{
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ''
+    }
+  },
+  created: function(){
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(res => {
+      this.pokemons = res.data.results
+      this.filteredPokemons = res.data.results
+    })
+  },
   components: {
-    HelloWorld
-  }
+   Pokemond
+  },
+   computed: {
+  //  resultadoBusca: function(){
+  //    if(this.busca == '' || this.busca == ' '){
+  //      return this.pokemons;
+  //    } else {
+  //      return this.pokemons.filter(pokemon => pokemon.name == this.busca)
+  //    }
+  //  }
+ },
+ methods: {
+   buscar: function(){
+     this.filteredPokemons = this.pokemons;
+     if(this.busca == '' || this.busca == ' '){
+       this.filteredPokemons = this.pokemons;
+     } else {
+       this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca);
+     }
+   }
+ }
 }
 </script>
+
+
+
 
 <style>
 #app {
@@ -24,5 +73,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.tittle {
+  color: #F9FCF3;
+  text-shadow: black 2px 4px 7px ;
+  font-family: 'Cabin', sans-serif;
+}
+.txtBuscar{
+  color: white;
+  font-family: 'Cabin', sans-serif;
+  text-shadow: black 2px 2px 2px ;
+}
+#input-default {
+  border-radius: 2em;
 }
 </style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,77 @@
+<template>
+  <b-container class="cardPokemon">
+          <b-card
+            no-body
+            style="max-width: 10rem; min-width: 200px;"
+            :img-src="currentImg"
+            img-alt="Image"
+            img-top
+            class="mr-auto ml-auto mb-4"
+          >
+            <template v-slot:header>
+              <h4 class="mb-0">{{name}}</h4>
+            </template>
+
+            <b-card-body>
+              <b-card-sub-title class="mb-2">{{pokemon.type}}</b-card-sub-title>
+            </b-card-body>
+        <b-list-group flush>
+      <b-list-group-item><b-button @click="mudarSprite" class="btnImg">Mudar Visualização</b-button></b-list-group-item>
+    </b-list-group>
+
+           
+          </b-card>
+  </b-container>
+</template>
+
+<script>
+
+import axios from 'axios';
+export default {
+  created: function(){
+    axios.get(this.url).then(res =>{
+      this.pokemon.type = res.data.types[0].type.name;
+      this.pokemon.front = res.data.sprites.front_default
+      this.pokemon.back = res.data.sprites.back_default
+      this.currentImg = this.pokemon.front
+    })
+  },
+  data(){
+    return {
+      isFront: true,
+      currentImg: '',
+      pokemon: {
+        type: "",
+        front: "",
+        back: ""      }
+    }
+  },
+  props: {
+    name: String,
+    url: String,
+    num: Number
+  },
+  filters: {
+  upper: function(value){
+    let newName = value[0].toUpperCase() + value.slice(1);
+    return newName
+ } },
+ methods: {
+   mudarSprite: function(){
+     if(this.isFront){
+       this.currentImg = this.pokemon.back;
+       this.isFront = false
+     } else {
+       this.isFront = true;
+       this.currentImg = this.pokemon.front;
+     }
+   }
+ }
+}
+</script>
+
+<style>
+  .btnImg{
+    background: #193441;
+  }
+</style>
